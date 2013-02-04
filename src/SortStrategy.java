616731aeed85a4b61601039ec10948219ee40b30
Rylan Dirksen <rylandirksen@gmail.com>
2013-02-04 11:06:11 -0600

    Updated swap() definition in interface

--- src/SortStrategy.java
+++ src/SortStrategy.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
+
 public interface SortStrategy<T> {
-    public void swap();
+    public void swap(int index, ArrayList collectionToSort, Object first, Object second);
     public boolean compare(T first, T second);
 }
