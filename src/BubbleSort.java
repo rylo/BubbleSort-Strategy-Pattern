16c87609c3b0fcc6cb0ccdb65665347aed1c896d
Rylan Dirksen <rylandirksen@gmail.com>
2013-02-04 11:05:23 -0600

    Moved all logic to BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,7 +1,36 @@
+import java.util.ArrayList;
+
 public class BubbleSort {
     private SortStrategy strategy;
 
-    public void setSortStrategy(SortStrategy) {
+    void setSortStrategy(SortStrategy strategy) {
+        this.strategy = strategy;
+    }
 
+    public void sort(ArrayList collectionToSort){
+        Object first, second;
+        boolean swapHappened = true;
+
+        while(swapHappened){
+            swapHappened = false;
+
+            for(int x = 0; x < (collectionToSort.size() - 1); x++) {
+                first = collectionToSort.get(x);
+                second = collectionToSort.get(x+1);
+
+                if (this.strategy.compare(first, second)) {
+                    this.strategy.swap(x, collectionToSort, first, second);
+                    swapHappened = true;
+                }
+            }
+        }
+        printArray(collectionToSort);
+    }
+
+    public final void printArray(ArrayList arrayToPrint){
+        System.out.println("Outcome:");
+        for(int x = 0; x < arrayToPrint.size(); x++) {
+            System.out.println(arrayToPrint.get(x));
+        }
     }
 }
